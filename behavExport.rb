d58c86c9a7f56972f5e66435f4d12440c0670eee
ubdbra001 <dan.t.brady21@gmail.com>
2022-01-28 21:54:47 +0000

    successfully transplanted innards from old script

--- behavExport.rb
+++ behavExport.rb
@@ -35,6 +35,71 @@ begin
         $db,proj = load_db(fullfile)
         puts "Loaded file"
 
+        column_list = getColumnList()
+        columns = Array.new
+
+        puts "Building header..."
+        header = Array.new
+        for col in column_list
+          col = getVariable(col)
+          args = col.arglist
+          header << col.name + ".ordinal"
+          header << col.name + ".onset"
+          header << col.name + ".offset"
+          header += args.collect { |arg| col.name + "." + arg }
+          columns << col
+        end
+    
+        # Get min and max times
+        puts "Getting the minimum and maximium times for the files..."
+        min = 99999999999999
+        max = 0
+        for col in columns
+          if col.cells.length > 0
+            lmin = col.cells[0].onset
+            lmax = col.cells[col.cells.length-1].offset
+            if lmin < min
+              min = lmin
+            end
+            if lmax > max
+              max = lmax
+            end
+          end
+        end
+    
+        # Now go from min to max time in 40ms intervals, getting the cell value at each time
+        puts "Getting the data for each time point..."
+        time = min
+        i = 0
+        total_i = (max - min) / frame_gap
+        output = "time" + ',' + header.join(',') + "\n"
+        while time <= max
+          output += time.to_s + ','
+          for col in columns
+            cell = getCellFromTime(col, time)
+            if cell != nil
+              data = printCellArgs(cell)
+              #puts data
+            else
+              data = [''] * (col.arglist.length + 3)
+            end
+            output += data.join(',') + ','
+          end
+          output += "\n"
+          time += frame_gap
+          i += 1
+          if i % 1000 == 0
+            puts "On row " + i.to_s + " out of " + total_i.to_s
+          end
+        end
+    
+        puts "Completed building data.  Writing to file " + outname
+        fo = File.new(outpath, 'a')
+        fo.write(output)
+        fo.flush()
+        fo.close()
+        puts "Finished" + filename
+
 
     end
 
