cb6e9267b9df1f31e35001ca7dac11a04e464f38
ubdbra001 <dan.t.brady21@gmail.com>
2022-01-28 21:38:22 +0000

    successfully loads files

--- behavExport.rb
+++ behavExport.rb
@@ -1,28 +1,41 @@
 require 'Datavyu_API.rb'
 
 begin
-    
-    project_dir ="~/Documents/Hana/behavExtract/" 
+    ## This needs editing to point to the correct dir
+    project_dir ="~/Documents/Hana/behavExtract/"
 
-    input_dir = project_dir + "data_in/"
-    output_dir = project_dir + "data_out/"    
+    frame_gap = 40
+    outfile_type = ".csv"
 
-    filedir = File.expand_path(input_dir)
-    filenames = Dir.new(filedir).entries
+
+    input_dir = File.expand_path(project_dir + "data_in/")
+    output_dir = File.expand_path(project_dir + "data_out/")   
+       
+
+    filenames = Dir.new(input_dir).entries
 
     for file in filenames
-        if file.include?(".opf") and file[0].chr != '.'
-        
-        filename = File.basename(file, ".opf")
-        outname = filename + ".txt"
-
-        puts "Loading: " + filename
-        $db,proj = load_db(filedir+file)
-        puts "Loaded"
-
-
-    
+        # Skip files that are not opf or are hidden
+        if !file.include?(".opf") or file[0].chr == '.'
+            next
         end
+        
+        # Generate full file path 
+        fullfile = File.join(input_dir, file)
+
+        # Extract filename
+        filename = File.basename(file, ".opf")
+        
+        # Generate outname and path
+        outname = filename + outfile_type
+        outpath = File.join(output_dir, outname)
+
+        # Load the data
+        puts "Loading: " + fullfile
+        $db,proj = load_db(fullfile)
+        puts "Loaded file"
+
+
     end
 
 end
